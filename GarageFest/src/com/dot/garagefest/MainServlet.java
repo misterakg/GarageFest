d5c582e346e242e1356fe55bed15635fdd9fe610
GauthamM-offical <mgautham1995@gmail.com>
2018-11-17 17:11:13 +0530

    Created two servlets for home page and "add new post". Changes to html and web.xml changed the directories of images,styles and scripts

---
+++ GarageFest/src/com/dot/garagefest/MainServlet.java
@@ -0,0 +1,48 @@
+package com.dot.garagefest;
+
+import java.io.IOException;
+
+import javax.jws.WebService;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MainServlet
+ */
+@WebService
+public class MainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public MainServlet() {
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+        request.getRequestDispatcher("/WEB-INF/index.html").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
